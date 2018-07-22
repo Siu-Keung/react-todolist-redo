bb2efae57134b275a2f3bb465b99c86d2f506375
SiuKeung <siu_keung@outlook.com>
2018-07-22 23:13:22 +0800

    完成了双击编辑的功能

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,20 +1,34 @@
 import React, {Component} from 'react';
 import './todo.css';
+import emitter from "./emitter";
 
 class ListItem extends Component {
     constructor(props) {
         super(props);
+        this.itemContentInput = React.createRef();
     }
 
     toggleCheckStatus = () => {
         this.props.onCheckBoxClicked(this.props.id);
     }
 
+    onEnterKeyUp(event){
+        if(event.keyCode === 13){
+            this.props.handleUpdateItem(this.props.id, this.itemContentInput.current.value);
+        }
+    }
+
+    onContentSpanClicked = () => {
+        this.props.handleContentSpanClicked(this.props.id);
+    }
+
     render() {
         let resultElem = (
             <li id={this.props.id} className={this.props.checked ? 'checked' : ''}>
                 <input onClick={this.toggleCheckStatus} name="done-todo" type="checkbox" className="done-todo" checked={this.props.checked}/>
-                <span>{this.props.content}</span>
+                {this.props.editable ?
+                    <input defaultValue={this.props.content} type='text' onKeyUp={this.onEnterKeyUp.bind(this)} ref={this.itemContentInput}/> :
+                    <span onDoubleClick={this.onContentSpanClicked}>{this.props.content}</span>}
             </li>
         );
         return resultElem;
--- src/ListItemGroup.js
+++ src/ListItemGroup.js
@@ -30,10 +30,10 @@ class ListItemGroup extends Component {
         super(props);
         this.state = {
             items: [
-                {id: '1', content: '做作业', checked: false, display: true},
-                {id: '2', content: '看电视', checked: false, display: true},
-                {id: '3', content: '打游戏', checked: true, display: true},
-                {id: '4', content: '看书', checked: false, display: true}
+                {id: '1', content: '做作业', checked: false, display: true, editable: false},
+                {id: '2', content: '看电视', checked: false, display: true, editable: false},
+                {id: '3', content: '打游戏', checked: true, display: true, editable: false},
+                {id: '4', content: '看书', checked: false, display: true, editable: false}
             ]
         };
         emitter.addListener('addItem', this.handleAddItemEvent);
@@ -53,24 +53,38 @@ class ListItemGroup extends Component {
 
     onFilterChanged = (newFilterName) => {
         switch (newFilterName) {
-            case '全部':
-                this.state.items.forEach(item => item.display = true);
-                break;
             case '未完成':
                 this.state.items.forEach(item => item.display = !item.checked);
                 break;
             case '已完成':
                 this.state.items.forEach(item => item.display = item.checked);
                 break;
+            default:
+                this.state.items.forEach(item => item.display = true);
+                break;
         }
         this.setState(this.state);
     }
 
+    handleUpdateItem = (id, newContent) => {
+        let targetItem = this.state.items.find(item => item.id === id);
+        targetItem.content = newContent;
+        targetItem.editable = false;
+        this.setState(this.state);
+    }
+
+    handleContentSpanClicked = (id) => {
+        this.state.items.find(item => item.id === id).editable = true;
+        this.setState(this.state);
+    }
 
     render() {
-        let itemList = this.state.items.filter(item => item.display).map(item =>
+        let itemList = this.state.items.filter(item => item.display !== false).map(item =>
             <ListItem id={item.id} content={item.content} checked={item.checked}
-                      onCheckBoxClicked={this.onCheckBoxClicked}/>);
+                      onCheckBoxClicked={this.onCheckBoxClicked}
+                      handleUpdateItem={this.handleUpdateItem}
+                      handleContentSpanClicked={this.handleContentSpanClicked}
+                      editable={item.editable}/>);
         return (
             <ol>
                 {itemList}
